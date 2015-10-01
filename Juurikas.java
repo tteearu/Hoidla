ecd6f3c1f256f14f14979d4bc208a2e1aca45649
tteearu <tteearu@itcollege.ee>
2015-10-01 17:49:07 +0300

    Kirjavea parandus

--- Juurikas.java
+++ Juurikas.java
@@ -5,7 +5,7 @@ import praktikum1.TextIO;
 public class Stringipikkus {
 	
 	public static void main(String[] args) {
-		System.out.print("Sisesta oma nimi ...asda ");		
+		System.out.print("Sisesta oma nimi ... ");		
 		String nimi = TextIO.getlnString();	
 		int nimePikkus = nimi.length();
 		System.out.println("Nimi "+nimi+" sisaldab "+nimePikkus +" tähemärki" );
